82113c7c2b3dd23d0d3e456c501406268d71cf19
Vincent <Kargos12@hotmail.fr>
2020-11-14 11:06:17 +0100

    Ajoutez des fichiers projet.

---
+++ Exercice 9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+/**
+ *Exercice 9
+ *Author : Kargos
+ *04/11/2020
+ */
+
+namespace Exercice_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Déclaration des variables
+            float Note;
+
+            //faire ca :
+            do
+            {
+                Console.Write("Saisissez une note entre 0 et 20 : ");
+                Note = float.Parse(Console.ReadLine());
+            }
+
+            // tant que ca :
+            while (Note>20 || Note<0);
+
+            //sinon :
+            Console.WriteLine("La note est de : " + Note);
+           
+            Console.ReadLine();
+        }
+        
+    }
+}
